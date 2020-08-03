4d71d3de939c323bdaa58dedcff25cde437bdc13
akeener97 <41094366+akeener97@users.noreply.github.com>
2020-08-03 16:14:34 -0400

    updated flash script

---
+++ docs/build_system/CMakeDetails.rst
@@ -0,0 +1,112 @@
+CMake Details
+*************
+
+Overview
+========
+
+CMake is used to build your application together with the mJackets API. A
+CMake build is done in two stages. The first stage is called
+**configuration**. During configuration, the CMakeLists.txt build scripts are
+executed. After configuration is finished, CMake has an internal model of the
+mJackets API build, and can generate build scripts that are native to the host
+platform.
+
+CMake supports generating scripts for several build systems, but only Make has
+tested to work with mJackets. After configuration, you begin the
+**build** stage by executing the generated build scripts. These build scripts
+can recompile the application without involving CMake following
+most code changes. However, after certain changes, the configuration step must
+be executed again before building. The build scripts can detect some of these
+situations and reconfigure automatically, but there are cases when this must be
+done manually.
+
+mJackets uses CMake's concept of a 'target' to organize the build. A
+target can be an executable, a library, or a generated file. For
+application developers, the library target is the most important to
+understand. All source code that goes into a mJackets build does so by
+being included in a library target, even application code.
+
+Library targets have source code, that is added through CMakeLists.txt
+build scripts like this:
+
+.. code-block:: cmake
+
+   target_sources(app PRIVATE src/main.c)
+
+In the above :file:`CMakeLists.txt`, an existing library target named ``app``
+is configured to include the source file :file:`src/main.c`. The ``PRIVATE``
+keyword indicates that we are modifying the internals of how the library is
+being built. Using the keyword ``PUBLIC`` would modify how other
+libraries that link with app are built. In this case, using ``PUBLIC``
+would cause libraries that link with ``app`` to also include the
+source file :file:`src/main.c`, behavior that we surely do not want. The
+``PUBLIC`` keyword could however be useful when modifying the include
+paths of a target library.
+
+Application CMakeLists.txt
+==========================
+
+Every application must have a :file:`CMakeLists.txt` file. This file is the
+entry point, or top level, of the build system. The final :file:`mJackets.elf`
+image contains the application and API libraries.
+
+This section describes some of what you can do in your top-level :file:`CMakeLists.txt`.
+file. Make sure to follow these steps in order.
+
+#. Add the model of your STM32 chip by setting the :makevar:`CHIP` variable. This
+will allow CMake to configure your project for your specific chip by including
+the appropriate drivers, and generate the approriate linker script and startup
+files.
+
+   .. code-block:: cmake
+
+      set(CHIP STM32F405RG)
+
+   Refer to :ref:`supported chips` for a complete list of supported chips.
+
+   The mJackets build system determines a value for :makevar:`CHIP` by checking
+   the following, in order (when a BOARD value is found, CMake stops looking
+   further down the list):
+
+   - Any previously used value as determined by the CMake cache takes highest
+     precedence. This ensures you don't try to run a build with a different
+     :makevar:`CHIP` value than you set during the build configuration step.
+
+   - Any value given on the CMake command line (directly or indirectly via
+     ``cmake build``) using ``-DCHIP=YOUR_CHIP`` will be checked for and
+     used next.
+
+   - If an :ref:`environment variable <env_vars>` ``CHIP`` is set, its value
+     will then be used.
+
+   - Finally, if you set ``CHIP`` in your application :file:`CMakeLists.txt`
+     as described in this step, this value will be used.
+
+#. Now add any application source files to the 'app' target
+   library, each on their own line, like so:
+
+   .. code-block:: cmake
+
+      target_sources(app PRIVATE src/main.c)
+
+Below is a simple example :file:`CMakeList.txt`:
+
+.. code-block:: cmake
+
+   project(my_app)
+   set(CHIP STM32F405RG)   
+
+   add_executable(app src/main.c)
+
+CMakeCache.txt
+==============
+
+CMake uses a CMakeCache.txt file as persistent key/value string
+storage used to cache values between runs, including compile and build
+options and paths to library dependencies. This cache file is created
+when CMake is run in an empty build folder.
+
+For more details about the CMakeCache.txt file see the official CMake
+documentation `runningcmake`_ .
+
+.. _runningcmake: http://cmake.org/runningcmake/
--- utilities/flash.py
+++ utilities/flash.py
@@ -1,28 +1,104 @@
-#!/usr/bin/env python3
-
+import argparse
+import logging
 import subprocess
 import sys
 import shlex
+import os.path
+import subprocess
+from tkinter import filedialog, Tk
 
-if len(sys.argv) < 2:
-    print("No file provided")
-    exit()
 
-fileName = sys.argv[1]
-flashAddress = "0x08000000"
+if sys.platform.startswith('linux'):
+    platform = "Linux"
+    findCmd = "which"
+elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+    platform = "Windows"
+    findCmd = "where"
+elif sys.platform.startswith('darwin'):
+    platform = "macOS"
+    findCmd = "which"
+    
+# Configure Command Line
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--mode", required=True, help="Flash programer to use", choices=["JLINK", "STLINK",  "UART", "USB", "SPI"])
+ap.add_argument("-t", "--target", required=True, help="Target STM32 chip")
+if platform == "Windows":
+    ap.add_argument("-b", "--binary", required=False, help="Full filename and path of firmware binary")
+else:
+    ap.add_argument("-b", "--binary", required=True, help="Full filename and path of firmware binary")
+ap.add_argument("-l", "--loglevel", required=False, help="Sets minimum log level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+ap.add_argument("-a", "--flashaddress", required=True, help="Starting address of flash to download firmware")
+args = ap.parse_args()
 
-testString = (
+# Configure Logging
+log_format = '%(message)s'
+if args.loglevel == None:
+    logging.basicConfig(format=log_format, level=logging.INFO)
+elif args.loglevel == "DEBUG":
+    logging.basicConfig(format=log_format, level=logging.DEBUG)
+elif args.loglevel == "INFO":
+    logging.basicConfig(format=log_format, level=logging.INFO)
+elif args.loglevel == "WARNING":
+    logging.basicConfig(format=log_format, level=logging.WARNING)
+elif args.loglevel == "ERROR":
+    logging.basicConfig(format=log_format, level=logging.ERROR)
+elif args.loglevel == "CRITICAL":
+    logging.basicConfig(format=log_format, level=logging.CRITICAL)
+
+# Verify that firmware binary is valid
+if args.binary == None or (not os.path.isfile(args.binary) and platform =="Windows"):
+    logging.warning("Firmware binary not specified. Please select binary file to upload")
+    root = Tk()
+    root.withdraw()
+    filename =  filedialog.askopenfilename(initialdir = "C:/",title = "Select binary file",filetypes=[("binary files","*.bin")])
+elif not os.path.isfile(args.binary):
+    logging.error("Firmware binary %s does not exist", args.binary)
+    sys.exit()
+else:
+    filename = args.binary
+
+# Check that flashing application exists and flash device
+logging.info("Flashing with %s", args.mode)
+if args.mode == "JLINK":
+    if subprocess.call([findCmd, 'JLinkExe']) != 0:
+        logging.error("JLinkExe application not installed or location not is not in PATH")
+        sys.exit()
+    jlink_flash(filename, args.target, args.flashadress)
+elif args.mode == "STLINK":
+    pass # TODO
+elif args.mode == "USB":
+    pass # TODO
+elif args.mode == "UART":
+    pass # TODO
+elif args.mode =="SPI":
+    pass # TODO
+    
+    
+def jlink_flash(filename, device, flash_address):
+    """Flashes a binary executable to an STM32 MCU using JLink
+
+    Args:
+        filename (str): Full name and path of the binary executable
+        device (str): STM32 device being flashed
+        flash_address (str): Starting flash address to upload program
+    """
+    ldcmd = (
     "r\n"
-    "loadbin \"" + fileName + "\" " + flashAddress + "\n"
+    "loadbin \"" + filename + "\" " + flash_address + "\n"
     "r\n"
     "g\n"
     "q\n"
-)
+    )
 
-p = subprocess.Popen(shlex.split("JLinkExe -device STM32F769NI -if JTAG -speed 4000 -jtagconf -1,-1 -autoconnect 1"), stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
-pOut = p.communicate(input=testString.encode())[0].decode()
-if 'Connecting to J-Link via USB...FAILED' in pOut or 'Cannot connect to J-Link via USB' in pOut:
-    sys.exit('Connection to J-Link failed')
-if 'Cannot connect to target' in pOut:
-    sys.exit('Connection to MCU failed')
-# TODO: more checks if something failed
+    cmdstr = "JLinkExe -device" + device + "-if JTAG -speed 4000 -jtagconf -1,-1 -autoconnect 1"
+    p = subprocess.Popen(shlex.split(cmdstr), stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p_out = p.communicate(input=ldcmd.encode())[0].decode()
+
+    if 'Connecting to J-Link via USB...FAILED' in p_out or 'Cannot connect to J-Link via USB' in p_out:
+        logging.error('Connection to J-Link failed')
+        sys.exit()
+    if 'Cannot connect to target' in p_out:
+        logging.error('Connection to MCU failed')
+        sys.exit()
+
+
