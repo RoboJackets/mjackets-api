e68bd945aa17570a7c8007801013b03902b2bddb
Austin Keener <akeener@outlook.com>
2020-09-19 20:13:39 -0400

    Systick functional

--- api/DigitalOut.cpp
+++ api/DigitalOut.cpp
@@ -1,3 +1,20 @@
+/**
+  ******************************************************************************
+  * @file           : DigitalOut.cpp
+  * @brief          : Digital Output API
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 RoboJackets.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by RoboJackets under Apache License
+  * 2.0; You may not use this file except in compliance with the License. You 
+  * may obtain a copy of the License at: 
+  *                    https://www.apache.org/licenses/LICENSE-2.0
+  ******************************************************************************
+  */
+
 #include "DigitalOut.hpp"
 
 DigitalOut::DigitalOut(PinName pin, PullType pull, PinMode mode, PinSpeed speed,
