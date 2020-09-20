25155c24ecd21e088ea75705533fe07bf81c9dae
CollinAvidano <collin.avidano@gmail.com>
2020-09-19 20:12:22 -0400

    look for startup at BSP_PATH

--- cmake/FindCMSIS.cmake
+++ cmake/FindCMSIS.cmake
@@ -62,36 +62,36 @@ find_path(CMSIS_CORE_COMMON_INCLUDE
     NO_DEFAULT_PATH
 )
 list(APPEND CMSIS_INCLUDE_DIRS "${CMSIS_CORE_COMMON_INCLUDE}")
-    
+
 function(cmsis_generate_default_linker_script FAMILY DEVICE)
-    stm32_get_memory_info(${FAMILY} ${DEVICE} 
-        FLASH_SIZE RAM_SIZE CCRAM_SIZE STACK_SIZE HEAP_SIZE 
+    stm32_get_memory_info(${FAMILY} ${DEVICE}
+        FLASH_SIZE RAM_SIZE CCRAM_SIZE STACK_SIZE HEAP_SIZE
         FLASH_ORIGIN RAM_ORIGIN CCRAM_ORIGIN
     )
     add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/STM32${DEVICE}.ld"
-        COMMAND ${CMAKE_COMMAND} 
-            -DFLASH_ORIGIN="${FLASH_ORIGIN}" 
-            -DRAM_ORIGIN="${RAM_ORIGIN}" 
-            -DCCRAM_ORIGIN="${CCRAM_ORIGIN}" 
-            -DFLASH_SIZE="${FLASH_SIZE}" 
-            -DRAM_SIZE="${RAM_SIZE}" 
-            -DCCRAM_SIZE="${CCRAM_SIZE}" 
-            -DSTACK_SIZE="${STACK_SIZE}" 
-            -DHEAP_SIZE="${HEAP_SIZE}" 
+        COMMAND ${CMAKE_COMMAND}
+            -DFLASH_ORIGIN="${FLASH_ORIGIN}"
+            -DRAM_ORIGIN="${RAM_ORIGIN}"
+            -DCCRAM_ORIGIN="${CCRAM_ORIGIN}"
+            -DFLASH_SIZE="${FLASH_SIZE}"
+            -DRAM_SIZE="${RAM_SIZE}"
+            -DCCRAM_SIZE="${CCRAM_SIZE}"
+            -DSTACK_SIZE="${STACK_SIZE}"
+            -DHEAP_SIZE="${HEAP_SIZE}"
             -DLINKER_SCRIPT="${CMAKE_CURRENT_BINARY_DIR}/STM32${DEVICE}.ld"
             -P "${MJACKETS_PATH}/cmake/stm32/linker_ld.cmake"
     )
     add_custom_target(CMSIS_LD_STM32_${DEVICE} DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/STM32${DEVICE}.ld)
     add_dependencies(CMSIS::STM32::${DEVICE} CMSIS_LD_STM32_${DEVICE})
     stm32_add_linker_script(CMSIS::STM32::${DEVICE} INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/STM32${DEVICE}.ld")
-endfunction() 
+endfunction()
 
 foreach(COMP ${CMSIS_FIND_COMPONENTS})
     string(TOLOWER ${COMP} COMP_L)
     string(TOUPPER ${COMP} COMP)
-    
+
     string(REGEX MATCH "^STM32([A-Z][0-9])([0-9A-Z][0-9][A-Z][0-9A-Z])?.*$" COMP ${COMP})
-    
+
     if((NOT CMAKE_MATCH_1) AND (NOT CMAKE_MATCH_2))
         message(FATAL_ERROR "Unknown CMSIS component: ${COMP}")
     endif()
@@ -102,7 +102,7 @@ foreach(COMP ${CMSIS_FIND_COMPONENTS})
         set(FAMILY ${CMAKE_MATCH_1})
         stm32_get_devices_by_family(${FAMILY} DEVICES)
     endif()
-    string(TOLOWER ${FAMILY} FAMILY_L) 
+    string(TOLOWER ${FAMILY} FAMILY_L)
 
     find_path(CMSIS_${FAMILY}_INCLUDE
         NAMES stm32${FAMILY_L}xx.h
@@ -117,9 +117,9 @@ foreach(COMP ${CMSIS_FIND_COMPONENTS})
         NO_DEFAULT_PATH
     )
     list(APPEND CMSIS_SOURCES "${CMSIS_${FAMILY}_SOURCE}")
-    
-    if ((NOT CMSIS_CORE_COMMON_INCLUDE) OR 
-        (NOT CMSIS_${FAMILY}_INCLUDE) OR 
+
+    if ((NOT CMSIS_CORE_COMMON_INCLUDE) OR
+        (NOT CMSIS_${FAMILY}_INCLUDE) OR
         (NOT CMSIS_${FAMILY}_SOURCE))
         continue()
     endif()
@@ -133,19 +133,19 @@ foreach(COMP ${CMSIS_FIND_COMPONENTS})
     endif()
 
     set(DEVICES_FOUND TRUE)
-    
+
     foreach(DEVICE ${DEVICES})
         stm32_get_chip_type(${FAMILY} ${DEVICE} TYPE)
         string(TOLOWER ${DEVICE} DEVICE_L)
         string(TOLOWER ${TYPE} TYPE_L)
-        
+
         find_file(CMSIS_${FAMILY}_${TYPE}_STARTUP
             NAMES startup_stm32${TYPE_L}.s
-            PATHS "${CMAKE_SOURCE_DIR}"
+            PATHS "${BSP_PATH}"
             NO_DEFAULT_PATH
         )
         if(CMSIS_${FAMILY}_${TYPE}_STARTUP)
-            message(STATUS "Using startup_stm32${TYPE_L}.s found in ${CMAKE_SOURCE_DIR}")
+            message(STATUS "Using startup_stm32${TYPE_L}.s found in ${BSP_PATH}")
         endif()
 
         if(NOT(CMSIS_${FAMILY}_${TYPE}_STARTUP))
@@ -162,20 +162,20 @@ foreach(COMP ${CMSIS_FIND_COMPONENTS})
             set(DEVICES_FOUND FALSE)
             break()
         endif()
-        
+
         if(NOT (TARGET CMSIS::STM32::${TYPE}))
             add_library(CMSIS::STM32::${TYPE} INTERFACE IMPORTED)
             target_link_libraries(CMSIS::STM32::${TYPE} INTERFACE CMSIS::STM32::${FAMILY} STM32::${TYPE})
             target_sources(CMSIS::STM32::${TYPE} INTERFACE "${CMSIS_${FAMILY}_${TYPE}_STARTUP}")
         endif()
-        
+
         add_library(CMSIS::STM32::${DEVICE} INTERFACE IMPORTED)
         target_link_libraries(CMSIS::STM32::${DEVICE} INTERFACE CMSIS::STM32::${TYPE})
         cmsis_generate_default_linker_script(${FAMILY} ${DEVICE})
     endforeach()
-        
-    if(CMSIS_CORE_COMMON_INCLUDE AND 
-       CMSIS_${FAMILY}_INCLUDE AND 
+
+    if(CMSIS_CORE_COMMON_INCLUDE AND
+       CMSIS_${FAMILY}_INCLUDE AND
        CMSIS_${FAMILY}_SOURCE AND
        DEVICES_FOUND)
        set(CMSIS_${COMP}_FOUND TRUE)
