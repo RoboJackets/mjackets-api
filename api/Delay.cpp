3c207b57693b3d50af0bd0efc2fbc0ed12320206
Austin Keener <akeener@outlook.com>
2020-09-19 20:34:24 -0400

    Modified DelayMs to use HAL_Delay

--- api/Delay.cpp
+++ api/Delay.cpp
@@ -37,8 +37,5 @@ void DelayUs(uint32_t us) // microseconds
 
 void DelayMs(uint32_t ms) // milliseconds
 {
-    volatile uint32_t delay_ticks = ms * (SystemCoreClock/1000L);
-    volatile uint32_t start_tick = DWT->CYCCNT;
-
-    do {} while (DWT->CYCCNT - start_tick < delay_ticks);
+    HAL_Delay(ms);
 }
