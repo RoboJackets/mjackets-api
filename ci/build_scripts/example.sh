4c7a3ef1880b70aaff5a0de3332a8d5a400942e8
akeener <akeener@outlook.com>
2020-09-23 14:31:08 -0400

    attempting to get sonar scanner to work

--- ci/build_scripts/example.sh
+++ ci/build_scripts/example.sh
@@ -1,9 +1,13 @@
+shopt -s dotglob
 git clone https://github.com/akeener97/mJackets-test2.git
 cd mJackets-test2
+ls
 git submodule init && git submodule update
 cd mjackets-api
 git submodule init && git submodule update
 cd ..
 cmake configure .
 make
-
+cd ..
+mv -rp mJackets-test2/* sonarqube-analysis-input  
+ls sonarqube-analysis-input  
