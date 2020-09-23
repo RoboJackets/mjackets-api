7256a8e7ebd5aaf6bbdf9431a342d40d320d6206
akeener <akeener@outlook.com>
2020-09-23 10:53:29 -0400

    got ci build script to run

--- ci/build_scripts/example.sh
+++ ci/build_scripts/example.sh
@@ -1,5 +1,9 @@
 git clone https://github.com/akeener97/mJackets-test2.git
-git submodule update
+cd mjackets-test2
+git submodule init && git submodule update
+cd mjackets-api
+git submodule init && git submodule update
+cd ..
 cmake configure .
 cmake build
 
