391e3f5a16e18caf15693dc2386ee7d7f4c02beb
akeener <akeener@outlook.com>
2020-09-26 19:43:47 -0400

    ci updates

--- ci/build_scripts/example.sh
+++ ci/build_scripts/example.sh
@@ -1,10 +1,3 @@
-wget -O cpp-build-wrapper.zip https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip \
-	&& unzip cpp-build-wrapper.zip \
-	&& export PATH=/$(pwd)/build-wrapper-linux-x86:$PATH \
-    && wget -O sonar-scanner-cli.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip \
-	&& unzip sonar-scanner-cli.zip \
-	&& export PATH=/$(pwd)/sonar-scanner-4.4.0.2170-linux/bin:$PATH
-
 git clone https://github.com/akeener97/mJackets-test2.git
 cd mJackets-test2
 git submodule init && git submodule update
@@ -16,7 +9,7 @@ cd ..
 cmake configure .
 
 # Build inside the build-wrapper
-build-wrapper-linux-x86-64 --out-dir bw-output make clean all
+build-wrapper-linux-x86-64 --out-dir bw-output make
 
 # Run sonar scanner
 sonar-scanner \
