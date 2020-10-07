03654e967b820b62fbded0d7915cb6a0b26557c0
Austin Keener <41094366+akeener97@users.noreply.github.com>
2020-10-07 17:18:25 -0400

    Added API documentation to resolve CI doc coverage failure (#28)

--- include/AnalogIn.hpp
+++ include/AnalogIn.hpp
@@ -2,7 +2,11 @@
 
 #include "mJackets.hpp"
 
-typedef struct ADCPinName {
+/**
+ * @brief Configuration struct to initialize a pin as an analog input
+ * 
+ */
+typedef struct {
     PinName pin_name;
     ADC_TypeDef* adc;
     uint32_t channel;
@@ -10,18 +14,70 @@ typedef struct ADCPinName {
 
 class AnalogIn {
   private:
-
+      /**
+       * @brief ADC_HandleTypeDef struct containing the ADC config parameters
+       * 
+       */
       ADC_HandleTypeDef ADC_InitStruct = {0};
+      
+      /**
+       * @brief Raw ADC value
+       * 
+       */
       uint32_t readValue;
+
+      /**
+       * @brief Unsigned 16-bit ADC value
+       * 
+       */
       uint16_t unsignedReadValue;
+
+      /**
+       * @brief Initializes the GPIO pin as an analog input
+       * 
+       * @param pin 
+       */
       void ADC_Init(ADCPinName pin);
 
    public:
+      /**
+       * @brief Construct a new Analog In object
+       * 
+       * @param pin 
+       */
       AnalogIn(ADCPinName pin);
+
+      /**
+       * @brief Destroy the Analog In object
+       * 
+       */
       ~AnalogIn();
 
+      /**
+       * @brief Returns the current ADC register value
+       * 
+       * @return uint32_t 
+       */
       uint32_t read();
+
+      /**
+       * @brief Returns the current 16-bit ADC register value
+       * 
+       * @return uint16_t 
+       */
       uint16_t read_u16();
+
+      /**
+       * @brief Returns the current voltage applied to the pin
+       * 
+       * @return float 
+       */
       float getVoltage();
+
+      /**
+       * @brief Returns the current ADC value scaled 0-100%
+       * 
+       * @return float 
+       */
       float getPercent();
 };
--- include/SPI.hpp
+++ include/SPI.hpp
@@ -3,34 +3,102 @@
 #include <vector>
 #include <optional>
 
-typedef enum SpiBus {
+/**
+ * @brief Typedef for all the SPI Bus peripherals
+ * 
+ */
+typedef enum {
     SpiBus2,
     SpiBus3,
     SpiBus5,
 } SpiBus;
 
+/**
+ * @brief SPI peripheral clock frequency
+ * 
+ */
 constexpr int fPCLK = 108'000'000; // Hz
 
 class SPI {
 public:
+    /**
+     * @brief Construct a new SPI object
+     * 
+     * @param spiBus 
+     * @param cs 
+     * @param hz 
+     */
     SPI(SpiBus spiBus, std::optional<PinName> cs = std::nullopt, int hz = 1'000'000);
+
+    /**
+     * @brief Destroy the SPI object
+     * 
+     */
     ~SPI();
 
+    /**
+     * @brief Configures the message format
+     * 
+     * @param bits 
+     * @param mode 
+     */
     void format(int bits, int mode = 0);
-    // TODO: make more clear is set in steps
+    
+    /**
+     * @brief Congfigures the bus frequency
+     * 
+     * @param hz Desired bus frequency
+     */
     void frequency(int hz);
 
+    /**
+     * @brief Transmits a byte onto the bus
+     * 
+     * @param data Byte to transmit onto the bus
+     */
     void transmit(uint8_t data);
+
+    /**
+     * @brief Transmits a byte array onto the bus
+     * 
+     * @param data 
+     */
     void transmit(const std::vector<uint8_t>& data);
+
+    /**
+     * @brief Transmits a byte array onto the bus
+     * 
+     * @param size Length of byte array to transmit
+     */
     void transmit(const uint8_t*, size_t size);
 
+    /**
+     * @brief Sequentially transmits and receives a byte
+     * 
+     * @param data Byte to transmit onto bus
+     * @return uint8_t Byte received from bus
+     */
     uint8_t transmitReceive(uint8_t data);
+
+    /**
+     * @brief Sequentially transmits and receives a byte array
+     * 
+     * @param data Byte array to transmit onto bus
+     * @return std::vector<uint8_t> Byte array received from bus
+     */
     std::vector<uint8_t> transmitReceive(const std::vector<uint8_t>& data);
+
+    /**
+     * @brief Sequentially transmits and receives a byte array
+     * 
+     * @param dataIn Pointer to data array to store recieved data
+     * @param dataOut Pointer to data array to tranmit onto the bus
+     * @param size Length of the receive and transmit arrays
+     */
     void transmitReceive(const uint8_t* dataIn, uint8_t* dataOut, size_t size);
 
 private:
     SPI_HandleTypeDef spiHandle = {};
     std::optional<PinName> chipSelect;
-
     uint32_t freqToPrescaler(int hz);
 };
